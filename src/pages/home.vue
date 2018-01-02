211705c4e288473168af11ae2c944f0fd9ac4e83
Ivan Fadila Putra <i.fadilaputra@gmail.com>
2018-01-03 05:06:42 +0700

    Fixing Sidebar :sparkles:

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,99 +1,87 @@
 <template>
-  <f7-page tabbar>
-    <f7-panel left layout="light">
-        <f7-navbar title="Kategori"></f7-navbar>
-            <f7-block>
-                <f7-list>
-                    <f7-list-item
-                        link="/home/"
-                        media="<img src='https://png.icons8.com/color/48/lego-head.png' title='Lego Head' width='48' height='48'>"
-                        title="Logo">
-                    </f7-list-item>
-                     <f7-list-item
-                        link="/home/"
-                        media="<img src='https://png.icons8.com/ios7/50/mobile-suit-gundam.png' title='Mobile Suit Gundam' width='50' height='50'>"
-                        title="Gundam">
-                    </f7-list-item>
-                    <f7-list-item
-                        link="/home/"
-                        media="<img src='https://png.icons8.com/color/50/000000/lego.png'>"
-                        title="Miniatur">
-                    </f7-list-item>
-                    <f7-list-item
-                        link="/home/"
-                        media="<img src='https://png.icons8.com/color/48/plush.png' title='Plush' width='48' height='48'>"
-                        title="Plush">
-                    </f7-list-item>
-                    <f7-list-item
-                        link="/home/"
-                        media="<img src='https://png.icons8.com/color/50/000000/lego.png'>"
-                        title="Action Figure">
-                    </f7-list-item>
-            </f7-list> 
-        </f7-block>   
-    </f7-panel>  
+    <div id="app">
+          <f7-panel left layout="light">
+                            <f7-navbar title="Kategori"></f7-navbar>
+                            <f7-block>
+                                <f7-list>
+                                    <f7-list-item link="/home/" media="<img style='width: 30px;' src='https://beon.kulelang.id/assets/images/category-icons/bricks.svg'>" title="Brick">
+                                    </f7-list-item>
+                                    <f7-list-item link="/home/" media="<img style='width: 30px;' src='https://beon.kulelang.id/assets/images/category-icons/actions.svg'>" title="Action Figure">
+                                    </f7-list-item>
+                                    <f7-list-item link="/home/" media="<img style='width: 30px;' src='https://beon.kulelang.id/assets/images/category-icons/automobile.svg'>" title="Model Kit">
+                                    </f7-list-item>
+                                    <f7-list-item link="/home/" media="<img style='width: 30px;' src='https://beon.kulelang.id/assets/images/category-icons/racing.svg'>" title="Plush">
+                                    </f7-list-item>
+                                    <f7-list-item link="/home/" media="<img style='width: 30px;' src='https://beon.kulelang.id/assets/images/category-icons/camera.svg'>" title="Card">
+                                    </f7-list-item>
+                                </f7-list>
+                            </f7-block>
+                        </f7-panel>
+        <f7-views>
+            <f7-view>
+                <f7-pages navbar-fixed toolbar-fixed>
+                    <f7-page>
+                      
+                        <f7-navbar>
+                            <f7-nav-left>
+                                <f7-button icon="icon-bars" open-panel="left"></f7-button>
+                            </f7-nav-left>
+                            <f7-nav-right class="padding-right">
+                                <f7-link href="/search/">
+                                    <f7-icon ion="search"></f7-icon>
+                                </f7-link>
+                                <f7-link href="/cart/">
+                                    <f7-icon ion="android-cart"></f7-icon>
+                                </f7-link>
+                            </f7-nav-right>
+                        </f7-navbar>
+                        <f7-tabs animated>
+                            <f7-tab id="home">
+                                <bid></bid>
+                            </f7-tab>
     
-    <f7-navbar>
-        <f7-nav-left>
-            <f7-button icon="icon-bars" open-panel="left"></f7-button>
-        </f7-nav-left>
-
-        <f7-nav-right class="padding-right">
-                <f7-link href="/search/">
-                    <f7-icon ion="search"></f7-icon>
-                </f7-link>
-                <f7-link href="/cart/">
-                    <f7-icon ion="android-cart"></f7-icon>
-                </f7-link> 
-        </f7-nav-right>
-    </f7-navbar>
-
-    <f7-tabs animated>
-      <f7-tab id="home">
-        <bid></bid>
-      </f7-tab>
-      
-      <f7-tab id="about">
-        <app-about></app-about>
-      </f7-tab>
-
-     <f7-tab id="profile">
-        <app-profile></app-profile>
-      </f7-tab>
-    </f7-tabs>
-
-    <f7-toolbar bottom tabbed tabbar inner>
-      <f7-link href="#home" tab-link active>
-          <f7-icon ion="android-home" size="24px"></f7-icon>
-      </f7-link>
-      <f7-link href="/process/" >
-          <f7-icon ion="android-time" size="24px"></f7-icon>
-      </f7-link>
-      <f7-link href="#about" tab-link>
-          <f7-icon ion="help-circled" size="24px"></f7-icon>
-      </f7-link>
-      <f7-link href="#profile" tab-link>
-          <f7-icon ion="android-person" size="24px"></f7-icon>
-      </f7-link>
-    </f7-toolbar>
-
-  </f7-page>
+                            <f7-tab id="about">
+                                <app-about></app-about>
+                            </f7-tab>
+    
+                            <f7-tab id="profile">
+                                <app-profile></app-profile>
+                            </f7-tab>
+                        </f7-tabs>
+                        <f7-toolbar bottom tabbed tabbar inner>
+                            <f7-link href="#home" tab-link active>
+                                <f7-icon ion="android-home" size="24px"></f7-icon>
+                            </f7-link>
+                            <f7-link href="/process/">
+                                <f7-icon ion="android-time" size="24px"></f7-icon>
+                            </f7-link>
+                            <f7-link href="#about" tab-link>
+                                <f7-icon ion="help-circled" size="24px"></f7-icon>
+                            </f7-link>
+                            <f7-link href="#profile" tab-link>
+                                <f7-icon ion="android-person" size="24px"></f7-icon>
+                            </f7-link>
+                        </f7-toolbar>
+                    </f7-page>
+                </f7-pages>
+            </f7-view>
+        </f7-views>
+    </div>
 </template>
 
 <script>
-
-import Bid from '../components/Bid'
-import About from '../components/About'
-import Profile from '../pages/profile/Profile'
-
-export default {
-    name : 'Home',
-    components : {
-        'bid': Bid,
-        'app-about': About,
-        'app-profile': Profile
-    },
-
-
-}
+    import Bid from '../components/Bid'
+    import About from '../components/About'
+    import Profile from '../pages/profile/Profile'
+    
+    export default {
+        name: 'Home',
+        components: {
+            'bid': Bid,
+            'app-about': About,
+            'app-profile': Profile
+        },
+    
+    
+    }
 </script>
